6f0b161cfc4715e45aac3c20b9ac28f8a5105270
phoshp <emre.yavuz.842@gmail.com>
2026-08-17 16:05:45 +0300

    fix2

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
-const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: isProd ? '/blog' : '', 
-  assetPrefix: isProd ? '/blog/' : '',
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
